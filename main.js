4fd7c623df77d6634e2d334a1143d285e307165a
Tiago Farinas <tiagofarinas@gmail.com>
2024-10-20 15:07:18 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
     sizeSelect.addEventListener('change', function(event) {// Update price based on size selection
         var selectedSize = event.target.value;
         productPrice.textContent = "Price: $" + pricing[selectedSize]})});// Update price 
+
+// Task 3: Handle Stock Availability
+var purchaseButton = document.getElementById('purchaseButton');
+var stock = {
+    small: true, // In Stock
+    medium: true, // In Stock
+    large: false };// Out of Stock
+sizeSelect.addEventListener('change', function(event) {
+    var selectedSize = event.target.value;
+    // Control purchase button based on stock availability
+    purchaseButton.disabled = !stock[selectedSize]}); // Disable if out of stock
